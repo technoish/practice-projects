ec1093d222b646433bbd1568a865ab306eb8ce3b
workdotnisha <workdotnisha@gmail.com>
2025-04-21 08:07:15 +0530

    updating project

--- python/snake-water-gun-game/main.py
+++ python/snake-water-gun-game/main.py
@@ -2,16 +2,26 @@ import random
 
 computer = random.choice([-1, 0, 1])
 youstr = input("Enter your choice (snake, water, gun): ").lower()
-# youDict = {"snake": -1, "water": 0, "gun": 1}
+youDict = {"snake": -1, "water": 0, "gun": 1}
 reverseDict = {-1: "snake", 0: "water", 1: "gun"}
-# you = youDict[youstr]
+you = youDict[youstr]
 
 print(f"You chose {reverseDict[you]}\nComputer chose {reverseDict[computer]}")
 
 if(computer == you):
     print("It's a tie!")
 else:
-    if((computer == -1 and you == 0) or (computer == 0 and you == 1) or (computer == 1 and you == -1)):
-        print("You lose!")
-    else:
+    if(computer == -1 and you == 1):
         print("You win!")
+    elif(computer == -1 and you == 0):
+        print("You lose!")
+    elif(computer == 0 and you == -1):
+        print("You win!")
+    elif(computer == 0 and you == 1):
+        print("You lose!")
+    elif(computer == 1 and you == -1):
+        print("You lose!")
+    elif(computer == 1 and you == 0):
+        print("You win!")
+    else:
+        print("Invalid input!")
